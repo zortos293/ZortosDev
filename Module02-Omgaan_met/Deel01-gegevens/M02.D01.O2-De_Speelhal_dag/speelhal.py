aa9e4df7e03a375cd5db52d58fbfdf883ddf0d8a
unknown <zortosgames293@gmail.com>
2022-09-05 12:07:54 +0200

    vijfde Commit

--- Module02-Omgaan_met/Deel01-gegevens/M02.D01.O2-De_Speelhal_dag/speelhal.py
+++ Module02-Omgaan_met/Deel01-gegevens/M02.D01.O2-De_Speelhal_dag/speelhal.py
@@ -1,17 +1,17 @@
 # De VIP-VR GameSeat kost per persoon 37 eurocent per 5 minuten
 # Dat kost een toegangsticket per persoon van 7,45 euro voor de hele dag
 
-ticket = 7.45
-vrseat = 0.37 
+Ticket = 7.45
+vr_seat = 0.37 
+Personen = 3
 
-
-Totaalticket = 7.45 *3
-print (f"Totaal kost de tickets : {Totaalticket}")
-vrseat1persoon = 0.37 * 9
-print (f"1 persoon vr seat voor 45 minuten kost : {vrseat1persoon}")
-vrseatTotaal = vrseat1persoon * 3
-print (f"3 personen vr seat voor 45 minuten kost : {vrseatTotaal}")
+Totaal_ticket = Ticket *3
+print (f"Totaal kost de tickets : {Totaal_ticket}")
+Vrseat_1_Persoon = vr_seat * 9
+print (f"1 persoon vr seat voor 45 minuten kost : {Vrseat_1_Persoon}")
+Totaal_Vrseat = Vrseat_1_Persoon * 3
+print (f"3 personen vr seat voor 45 minuten kost : {Totaal_Vrseat}")
 print (f"==============================================")
-Totaalprijs = vrseatTotaal + Totaalticket
-print (f"In totaal kost het  : {Totaalprijs}")
-print (f"Dit geweldige dagje-uit met 4 mensen in de Speelhal met 45 minuten VR kost je maar {Totaalprijs} euro")
+Totaal_prijs = Totaal_Vrseat + Totaal_ticket
+print (f"In totaal kost het  : {Totaal_prijs}")
+print (f"Dit geweldige dagje-uit met {Personen} mensen in de Speelhal met 45 minuten VR kost je maar {Totaal_prijs} euro")
