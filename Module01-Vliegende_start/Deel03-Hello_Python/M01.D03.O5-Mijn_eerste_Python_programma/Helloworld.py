f55eefd86843ae2f1e64a2354e0eae19ce0b4171
unknown <zortosgames293@gmail.com>
2022-09-05 11:31:28 +0200

    Tweede commit!

---
+++ Module01-Vliegende_start/Deel03-Hello_Python/M01.D03.O5-Mijn_eerste_Python_programma/Helloworld.py
@@ -0,0 +1,11 @@
+# hello world
+# Naam : Emin
+# StudentNummer : 99072620
+print("Hello wereld van…                                    ")
+print("    pppp    y     y   ttttttt   h   h    oooo   nnnn       ")
+print("    p   p    y   y       t      h   h   o    o  n   n")
+print("    p   p     y y        t      h   h   o    o  n   n")
+print("    pppp       y         t      hhhhh   o    o  n   n")
+print("    p          y         t      h   h   o    o  n   n")
+print("    p          y         t      h   h   o    o  n   n")
+print("    p          y         t      h   h    oooo   n   n")
