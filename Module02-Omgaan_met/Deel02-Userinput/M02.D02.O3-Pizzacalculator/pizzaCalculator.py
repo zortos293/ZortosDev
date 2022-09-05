3262146525d0853a0c72d6d9ac3c94922a727464
unknown <zortosgames293@gmail.com>
2022-09-05 15:32:21 +0200

    Negenste commit!!

---
+++ Module02-Omgaan_met/Deel02-Userinput/M02.D02.O3-Pizzacalculator/pizzaCalculator.py
@@ -0,0 +1,33 @@
+import math
+def round_up(n, decimals=0):
+    multiplier = 10 ** decimals
+    return math.ceil(n * multiplier) / multiplier
+pizza_small_prijs = 11.60
+pizza_medium_prijs = 19.40
+pizza_large_prijs = 24.70
+pizza_small_amount = int(input("[-] Hoeveel small pizza's wilt u hebben? : "))
+pizza_medium_amount = int(input("[-] Hoeveel medium pizza's wilt u hebben? : "))
+pizza_large_amount = int(input("[-] Hoeveel Large pizza's wilt u hebben? : "))
+pizza_small_total_price = pizza_small_prijs * pizza_small_amount
+pizza_medium_total_price = pizza_medium_prijs * pizza_medium_amount
+pizza_large_total_price = pizza_large_prijs * pizza_large_amount
+pizza_total_price = pizza_small_total_price + pizza_medium_total_price + pizza_large_total_price
+
+
+print (f"""
+U heeft :
+=========================================
+{pizza_small_amount} Small pizza ({round_up(pizza_small_total_price ,3 )}€)
+{pizza_medium_amount} Medium pizza ({round_up(pizza_medium_total_price ,3 )}€)
+{pizza_large_amount} Large pizza ({round_up(pizza_large_total_price ,3 )}€)
+==========================================
+In Totaal kost het : ({pizza_total_price}€)
+""")   
+input("press enter key to exit")
+
+
+
+
+
+
+
