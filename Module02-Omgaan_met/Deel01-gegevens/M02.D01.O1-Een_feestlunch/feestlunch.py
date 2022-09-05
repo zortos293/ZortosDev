75fc7c21b9773d724f8d662a7a16bc062f8425b3
unknown <zortosgames293@gmail.com>
2022-09-05 11:48:12 +0200

    Derde Commit

---
+++ Module02-Omgaan_met/Deel01-gegevens/M02.D01.O1-Een_feestlunch/feestlunch.py
@@ -0,0 +1,14 @@
+crossant = 0.39
+stokbrood = 2.78
+korting = 0.50
+
+
+print (crossant *17 + stokbrood *2 )
+totaalprijs = crossant *17 + stokbrood *2
+# in totaal prijs
+print (f"Prijs in totaal is : {totaalprijs}")
+totaalmetkorting = totaalprijs - korting *3
+# prijs met korting
+print (f"prijs met Korting is : {totaalmetkorting}")
+
+
