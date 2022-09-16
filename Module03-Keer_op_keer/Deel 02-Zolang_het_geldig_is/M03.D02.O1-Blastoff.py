e1a6842a9d42508a6bfa986ec11f990010c61db5
Zortos <zortosgames293@gmail.com>
2022-09-16 10:39:28 +0200

    M03.D02.O2 - De tijd

--- Module03-Keer_op_keer/Deel 02-Zolang_het_geldig_is/M03.D02.O1-Blastoff.py
+++ Module03-Keer_op_keer/Deel 02-Zolang_het_geldig_is/M03.D02.O1-Blastoff.py
@@ -1,4 +1,4 @@
 x = 0
-while x != 30:
+while x <= 30:
     print(x +1)
     x = x + 1
---
+++ Module03-Keer_op_keer/Deel 02-Zolang_het_geldig_is/M03.D02.O2-De_tijd.py
@@ -0,0 +1,9 @@
+x = 1
+y = 1
+while x <= 12:
+    print(f'{x}AM')
+    x = x +1
+
+while y <= 12:
+    print(f'{y }PM')  
+    y = y + 1
