869375ccb910c694524825fc2f87135675ee6db5
Zortos <zortosgames293@gmail.com>
2022-10-03 12:39:40 +0200

    boodschappenlijstje

--- Module04-Lijstjes_en_samenstellingen/Deel 01-Collections/M04.D01.O3-Deck_van_speelkaarten.py
+++ Module04-Lijstjes_en_samenstellingen/Deel 01-Collections/M04.D01.O3-Deck_van_speelkaarten.py
@@ -1,18 +1,22 @@
-from random import randint, random
+from random import randint, random,shuffle
+from re import L
 
 deck_kleuren = ('harten', 'klaveren', 'schoppen' , 'ruiten')
 deck_kaarten = (2,3,4,5,6,7,8,9,10,'een boer','een vrouw','een heer','een aas')
 deck = []
 joker_amount = 0
-for z in range(45):
-    randomint = randint(1,5)
-    deck.append(deck_kleuren[randint(0,3)] + ' ' +str(deck_kaarten[randint(1,12)]))
-    if randomint == 4:
-        if joker_amount != 2:
-            deck.append('joker')
-            joker_amount = joker_amount + 1
 
+for l in range(4):
+    for p in range(13):
+        deck.append(deck_kleuren[l] + ' ' + str(deck_kaarten[p]))
+
+for z in range(2):
+    deck.append('joker')    
+
+print(deck)
+shuffle(deck)
 
 for p in range(7):
     print(f'Kaart {p + 1}: {deck[p]}')
+
 print(f'Deck {len(deck)} Kaarten: {deck}') 
