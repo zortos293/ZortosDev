bd8b9647b36b5c7c5c4a5404bac46d58d881d00a
Zortos <zortosgames293@gmail.com>
2022-10-03 15:13:53 +0200

    update 2

--- Module04-Lijstjes_en_samenstellingen/Deel 01-Collections/M04.D01.O5-Boodschappenlijstje.py
+++ Module04-Lijstjes_en_samenstellingen/Deel 01-Collections/M04.D01.O5-Boodschappenlijstje.py
@@ -1,30 +1,18 @@
 my_dict = {}
 show_lijst = False
-first_time = False
 
 while show_lijst == False:
-    if first_time == False:
-        first_time = True
-        product = input("Product naam: ")
-        productint = input("Product stuks: ")
-        if product.lower() in my_dict:
-            
-            my_dict[product.lower()] = my_dict[product.lower()] + int(productint)
-        else:
-            my_dict[product.lower()] = int(productint)
+    product = input("Product naam: ")
+    productint = input("Product stuks: ")
+    if product.lower() in my_dict:
+        my_dict[product.lower()] = my_dict[product.lower()] + int(productint)
     else:
-        w = input("wil je meer Producten toevoegen?")    
-        if w == 'y'.lower():
-            product = input("Product naam: ")
-            productint = input("Product stuks: ")
-            if product.lower() in my_dict:
-
-                my_dict[product.lower()] = my_dict[product.lower()] + int(productint)
-            else:
-                my_dict[product.lower()] = int(productint)
-        else:
-            show_lijst = True
-            print(f'\n\n-[BOODSCHAPPENLIJST]-')
-            for key,value in my_dict.items():
-                print(f'{value}x {key}')
-            print(f'--------------------')
+        my_dict[product.lower()] = int(productint)
+    w = input("wil je meer Producten toevoegen?")    
+    if w != 'y'.lower():
+        break
+    
+print(f'\n-[BOODSCHAPPENLIJST]-')
+for key,value in my_dict.items():
+    print(f'{value}x {key}')
+print(f'--------------------')
