37154966dbb8f48f6af289c64716404cace0a22c
Zortos <zortosgames293@gmail.com>
2022-10-08 18:11:29 +0200

    M04.D02.O11 - Rond vs niet rond van de beschikbare kleuren

---
+++ Module04-Lijstjes_en_samenstellingen/Deel02-[fruit]/M04.D02.O11-Rond_vs_niet_rond_van_de_beschikbare_kleuren.py
@@ -0,0 +1,31 @@
+from fruitmand import fruitmand
+question_1 = input("Vul uw kleur in : ")
+round_true = 0
+round_false = 0
+
+def check_color():
+    for fruit in fruitmand:
+            if fruit['color'] in question_1:
+                return True
+    return False
+
+
+while True:
+    if check_color():
+        question_1 = input("Vul uw kleur in : ")
+        for fruit in fruitmand:
+            if fruit['color'] in question_1:
+                if fruit['round'] == True:
+                    round_true += 1
+                else:
+                    round_false += 1
+
+        if round_true == round_false:
+            print(f"Er zijn {round_true} ronde vruchten en {round_false} niet ronde vruchten in de kleur {question_1}")
+        elif round_true > round_false:
+            print(f"Er zijn {round_true - round_false} meer ronde vruchten dan niet ronde vruchten in de kleur {question_1}")
+        elif round_true < round_false:
+            print(f"Er zijn {abs(round_true - round_false)} minder ronde vruchten dan niet ronde vruchten in de kleur {question_1}")    
+        exit()
+    else:
+        print("De kleur {question_1} zit er niet in de fruitmand")
