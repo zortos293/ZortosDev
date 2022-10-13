c48f7a849ef35d372334bef6ef5cd0890bce0a6a
Zortos <zortosgames293@gmail.com>
2022-10-13 09:49:22 +0200

    M05.D01.O2 - Hello from function towns!?

--- Module05-Meer_Fun(ctions)/Deel 01-De_functie_van_Functions/hello.py
+++ Module05-Meer_Fun(ctions)/Deel 01-De_functie_van_Functions/hello.py
@@ -1,7 +1,14 @@
-def print_hello():
-    return "Hello from function town"
 
 
-print(print_hello())
+def print_hello(loop :int):
+    output = ""
+    for i in range(loop):
+       output += f"\nHello from function town {i+1}!"
+    return output
+
+
+print(print_hello(3))
+print(print_hello(7))
+
 
 
