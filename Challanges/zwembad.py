057236fcea6021f418a70b3e237f1a77a1b27284
Zortos <zortosgames293@gmail.com>
2022-10-13 09:27:40 +0200

    Stap2

--- Challanges/zwembad.py
+++ Challanges/zwembad.py
@@ -1,3 +1,9 @@
 zwembad_meters = 8*3*1.5
+zwembad_uitgraven = zwembad_meters*25
+zwembad_afvoeren = zwembad_meters*32.5
+zwembad_totaal = round(zwembad_uitgraven + zwembad_afvoeren)
 
-print(zwembad_meters)
+print(f"Offerte van een zwembed van 8x3x1.5 meter (inhoud {zwembad_meters} m3)")
+print(f"Uitgraven : €{zwembad_uitgraven} ")
+print(f"Afvoeren Grond : €{zwembad_afvoeren} ")
+print(f"Totaal : €{zwembad_totaal} ")
