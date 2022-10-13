d085255382deaf1724b93df52ae5a4b7db32d21f
Zortos <zortosgames293@gmail.com>
2022-10-13 09:33:30 +0200

    stap4

--- Challanges/zwembad.py
+++ Challanges/zwembad.py
@@ -1,10 +1,16 @@
-zwembad_meters = 8*3*1.5
+zwembad_breedte = float(input("Geef de breedte van het zwembad in: "))
+zwembad_lengte = float(input("Geef de lengte van het zwembad in: "))
+zwembad_hoogte = float(input("Geef de hoogte van het zwembad in: "))
+
+
+
+zwembad_meters = zwembad_lengte*zwembad_breedte*zwembad_hoogte
 zwembad_uitgraven = zwembad_meters*25
 zwembad_afvoeren = zwembad_meters*32.5
 zwembad_voorijkosten = 250 + round(zwembad_meters * 2.05)
 zwembad_totaal = round(zwembad_uitgraven + zwembad_afvoeren + zwembad_voorijkosten)
 
-print(f"Offerte van een zwembed van 8x3x1.5 meter (inhoud {zwembad_meters} m3)")
+print(f"Offerte van een zwembed van {zwembad_breedte}bij {zwembad_lengte} bij {zwembad_hoogte} meter (inhoud {zwembad_meters} m3)")
 print(f"Uitgraven : €{zwembad_uitgraven} ")
 print(f"Afvoeren Grond : €{zwembad_afvoeren} ")
 print(f"Voorrijkosten : €{zwembad_voorijkosten} ")
