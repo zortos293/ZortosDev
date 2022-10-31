762a86797225ae580de9ab7122d31dafdf7b1584
Zortos <zortosgames293@gmail.com>
2022-10-31 11:15:40 +0100

    multiple choice

---
+++ Challanges/Multiple_choice.py
@@ -0,0 +1,17 @@
+print("""
+Hoe kun je het beste een comment beschrijven?
+
+a) een stuk text die je kan terug kijken in de code
+b) een stuk code die je kan gebruiken
+c) een variable die je kan bekijken
+d) een int die je kan gebruiken in code
+""")    
+letter =  input("> ").lower()
+if letter == "a":
+    print("goede antwoord goed gedaan!!")
+elif letter == "b":
+    print("Foute Antwoord :( ")
+elif letter == "c":
+    print("Foute Antwoord :( ")
+elif letter == "d":
+    print("Foute Antwoord :( ")
