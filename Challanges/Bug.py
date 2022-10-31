ca8c6623b7fa6444c4dea2b86777110274fef673
Zortos <zortosgames293@gmail.com>
2022-10-31 11:31:43 +0100

    Bugs <<<

---
+++ Challanges/Bug.py
@@ -0,0 +1,15 @@
+import random
+print("Num Generator made by ZZZortos")
+num1 = input("Nummer 1 : ")
+num2 = input("Nummer 2 : ")
+
+def nummerfixer(num1 , num2 )
+    print(random.randint((num1 + num2), (num1 + num2)*10))
+
+
+
+if nummerfixer(num1 , num2)  => 1000 :
+    print("Nummer Generator Output : is boven 1000 !!")
+else
+    print("Nummer Generator Output {nummerfixer(num1,num2)}")  
+
