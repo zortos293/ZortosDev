d87bfff8212a17ece7645e857af4fc12b5bf8ba4
Zortos <zortosgames293@gmail.com>
2023-03-13 09:20:01 +0100

    M02.O1 - Buttons 1.0

--- Dynamische_User_Interfaces/Deel-02/buttons-template/buttons.js
+++ Dynamische_User_Interfaces/Deel-02/buttons-template/buttons.js
@@ -3,17 +3,38 @@ var button2 = document.getElementById("button2");
 var button3 = document.getElementById("button3");
 var container = document.getElementById("container");
 var image = document.getElementById("image");
-let a = 0;
+var image1 = document.getElementById("image1");
+let a = 1;
+let reverse = false;
 // A function that changes the background image of the container and the image in the HTML
 function changeBackgroundAndImage(url) {
-if (a => 3) {
-  image.src = "images/" + url + ".jpg";
-  a+=1;
   
-}
-image.src = "images/" + url + ".jpg";
-a += 1;
-}
+  if (reverse == true) {
+    if (a == 1) {
+      reverse = false;
+    }
+    else {
+
+      a -= 1;
+      image.src = "images/bg" + a + ".jpg";
+      image1.src = "images/" + a + ".jpg";
+    }
+  }
+  else {
+    if (a == 3) {
+      reverse = true;
+      image.src = "images/bg" + a + ".jpg";
+      image1.src = "images/" + a + ".jpg";
+    }
+    else{
+      image.src = "images/bg" + a + ".jpg";
+      image1.src = "images/" + a + ".jpg";
+      a += 1;
+    }
+  }
+
+  console.log(a);
+  }
 
 // A function that increases the value of a button by one
 function increaseValue(button) {
@@ -31,13 +52,13 @@ function increaseValue(button) {
 // Add event listeners to each button to increase their value on click
 button1.addEventListener("click", function() {
   increaseValue(button1);
-  changeBackgroundAndImage(a);
+  changeBackgroundAndImage(a,reverse);
 });
 button2.addEventListener("click", function() {
   increaseValue(button2);
-  changeBackgroundAndImage(a);
+  changeBackgroundAndImage(a,reverse);
 });
 button3.addEventListener("click", function() {
   increaseValue(button3);
-  changeBackgroundAndImage(a);
+  changeBackgroundAndImage(a,reverse);
 });
